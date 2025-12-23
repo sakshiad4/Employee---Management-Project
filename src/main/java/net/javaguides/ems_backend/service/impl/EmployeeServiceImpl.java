cc9e8683065674fbb1685c44f468c3b1ccdb7bdd
sakshiad4 <sakshiwork145@gmail.com>
2025-12-23 14:33:48 +0530

    Add files via upload

---
+++ src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,43 @@
+package net.javaguides.ems_backend.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourseNotFoundException;
+import net.javaguides.ems_backend.mapper.EmployeeMapper;
+import net.javaguides.ems_backend.repository.EmployeeRepository;
+import net.javaguides.ems_backend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.maptoEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.maptoEmployeeDto(savedEmployee);
+
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourseNotFoundException("Employee does not exist with ID:"+employeeId));
+        return EmployeeMapper.maptoEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::maptoEmployeeDto)
+                .collect(Collectors.toList());
+    }
+}
